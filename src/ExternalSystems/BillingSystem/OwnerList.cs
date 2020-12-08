1ee06529e32e65fef89b9d6842c0d03ce9700cfa
Kathleen Dollard <kdollard@microsoft.com>
2020-12-08 10:23:23 -0800

    Updated OwnerList

--- src/ExternalSystems/BillingSystem/OwnerList.cs
+++ src/ExternalSystems/BillingSystem/OwnerList.cs
@@ -7,13 +7,27 @@ namespace TollCollectorLib.BillingSystem
 {
     public class OwnerList
     {
-        private Dictionary<string, Owner> owners;
+        private readonly Dictionary<string, Owner> owners;
 
-        public OwnerList()
-        { }
-
-        public bool TryLookupOwner(string id, out Owner owner)
+        private OwnerList(Dictionary<string, Owner> dictionary)
         {
+            owners = dictionary;
+        }
+
+        public static OwnerList FetchOwners()
+        {
+            return new OwnerList(new Dictionary<string, Owner>
+                            {
+                                { "ID-TRE-LK",  new Owner( "Suzanne",  "Wise")},
+                                { "AK-MNK-LIY",  new Owner( "Bill",  "Nye") },
+                                { "WA-LLIIJJK",  new Owner( "Joe",  "Blue") }
+                            }
+                         );
+        }
+
+        public bool TryLookupOwner(string state, string plate, out Owner owner)
+        {
+            var id = $"{state}-{plate}";
             if (owners.TryGetValue(id, out owner))
             {
                 if (owner != null)  // show both ! is null and is not null
