b2b281573670418538896028ac3be46adc324bc0
Kathleen Dollard <kdollard@microsoft.com>
2020-12-08 10:56:09 -0800

    Binary literals LogLevel

--- src/Common/LogLevel.cs
+++ src/Common/LogLevel.cs
@@ -5,8 +5,10 @@ namespace Common
     [Flags]
     public enum LogLevel
     {
-        Info,
-        Error,
-        Warning
+        Info=0b0000,
+        Warning = 0b0010,
+        Error = 0b0100,
+        ReallyBad = 0b0100_0000_0000,
+        Worse = 0b1000_0000_0000,
     }
 }
