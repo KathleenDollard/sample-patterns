25dfe8b204e74e5f705511bf73fe89f0a555736d
Kathleen Dollard <kdollard@microsoft.com>
2020-12-08 10:39:55 -0800

    Simplification done

--- src/Common/LogLevel.cs
+++ src/Common/LogLevel.cs
@@ -6,7 +6,9 @@ namespace Common
     public enum LogLevel
     {
         Info,
-        Warning = 2,
-        Error = 4,
+        Warning = 0b0010,
+        Error = 0b0100,
+        ReallyBad = 0b0100_0000_0000,
+        Worse = 0b100_00000_0000,
     }
 }
