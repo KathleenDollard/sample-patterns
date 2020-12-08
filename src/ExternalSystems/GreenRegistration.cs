24e1920672c83a36664c4d7b90b44c6b65e8734e
Fredric Silberberg <frsilb@microsoft.com>
2020-12-07 16:49:37 -0800

    Convert the registration types to fully-expanded record types.

--- src/ExternalSystems/GreenRegistration.cs
+++ src/ExternalSystems/GreenRegistration.cs
@@ -1,18 +1,50 @@
-﻿namespace GreenRegistration
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenRegistration
 {
-    public class temp { }
-    //public readonly struct Cycle
-    //{
-    //    public Cycle(int riders, int wheels)
-    //    {
-    //        Riders = riders;
-    //        Wheels = wheels;
-    //    }
+    public class Cycle : IEquatable<Cycle>
+    {
+        public Cycle(int riders, int wheels)
+        {
+            Riders = riders;
+            Wheels = wheels;
+        }
 
-    //    public void Deconstruct(out int riders, out int wheels) =>
-    //            (riders, wheels) = (Riders, Wheels);
+        public int Riders { get; }
+        public int Wheels { get; }
 
-    //    public int Riders { get; }
-    //    public int Wheels { get; }
-    //}
+        public void Deconstruct(out int riders, out int wheels)
+        {
+            riders = Riders;
+            wheels = Wheels;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Cycle);
+        }
+
+        public bool Equals(Cycle other)
+        {
+            return other != null &&
+                   Riders == other.Riders &&
+                   Wheels == other.Wheels;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Riders, Wheels);
+        }
+
+        public static bool operator ==(Cycle left, Cycle right)
+        {
+            return EqualityComparer<Cycle>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Cycle left, Cycle right)
+        {
+            return !(left == right);
+        }
+    }
 }
--- src/ExternalSystems/LiveryRegistration.cs
+++ src/ExternalSystems/LiveryRegistration.cs
@@ -1,19 +1,86 @@
-﻿namespace LiveryRegistration
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiveryRegistration
 {
-    public class Taxi
+    public class Taxi : IEquatable<Taxi>
     {
-        public int Fares { get; set; }
+        public Taxi(int fares)
+        {
+            Fares = fares;
+        }
+
+        public int Fares { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Taxi);
+        }
+
+        public bool Equals(Taxi other)
+        {
+            return other != null &&
+                   Fares == other.Fares;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Fares);
+        }
+
+        public static bool operator ==(Taxi left, Taxi right)
+        {
+            return EqualityComparer<Taxi>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Taxi left, Taxi right)
+        {
+            return !(left == right);
+        }
     }
 
-    public class Bus
+    public class Bus : IEquatable<Bus>
     {
-        public int Capacity { get; set; }
-        public int Riders { get; set; }
+        public Bus(int capacity, int riders)
+        {
+            Capacity = capacity;
+            Riders = riders;
+        }
+
+        public int Capacity { get; }
+        public int Riders { get; }
 
         public void Deconstruct(out int capacity, out int riders)
         {
             capacity = Capacity;
             riders = Riders;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Bus);
+        }
+
+        public bool Equals(Bus other)
+        {
+            return other != null &&
+                   Capacity == other.Capacity &&
+                   Riders == other.Riders;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Capacity, Riders);
+        }
+
+        public static bool operator ==(Bus left, Bus right)
+        {
+            return EqualityComparer<Bus>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Bus left, Bus right)
+        {
+            return !(left == right);
         }
     }
 }
