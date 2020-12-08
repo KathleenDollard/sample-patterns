13b50eda6263dd68a375f4aaa1ce7c1cf9356db6
Kathleen Dollard <kdollard@microsoft.com>
2020-12-08 12:38:12 -0800

    Fix

--- src/TollCollectorLib/TollSystem.cs
+++ src/TollCollectorLib/TollSystem.cs
@@ -10,24 +10,8 @@ namespace TollCollectorLib
 {
     public static class TollSystem
     {
+
         private static readonly ConcurrentQueue<(object vehicle, DateTime time, bool inbound, string license)> s_queue
-           = new();
-            public readonly object Vehicle;
-            public readonly DateTime Time;
-            public readonly bool Inbound;
-            public readonly string License;
-
-            public QueueEntry(object vehicle, DateTime time, bool inbound, string license)
-            {
-                Vehicle = vehicle;
-            var item = (vehicle, time, inbound, license);
-            s_queue.Enqueue(item);
-                Inbound = inbound;
-                License = license;
-            }
-        }
-
-        private static readonly ConcurrentQueue<QueueEntry> s_queue
             = new();
         private static ILogger s_logger;
 
@@ -37,7 +21,7 @@ namespace TollCollectorLib
         public static void AddEntry(object vehicle, DateTime time, bool inbound, string license)
         {
             s_logger.SendMessage($"{time}: {(inbound ? "Inbound" : "Outbound")} {license} - {vehicle}");
-            s_queue.Enqueue(new QueueEntry(vehicle, time, inbound, license));
+            s_queue.Enqueue((vehicle, time, inbound, license));
         }
 
         public static async Task ChargeTollAsync(
