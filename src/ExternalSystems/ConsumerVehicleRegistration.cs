e785055830f735a685f5ec34d8fbc802425deda3
Kathleen Dollard <kdollard@microsoft.com>
2020-12-08 17:12:12 -0800

    Updated registrations to constructors

--- src/ExternalSystems/ConsumerVehicleRegistration.cs
+++ src/ExternalSystems/ConsumerVehicleRegistration.cs
@@ -1,7 +1,46 @@
-﻿    namespace ConsumerVehicleRegistration
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsumerVehicleRegistration
+{
+    public class Car : IEquatable<Car>
     {
-        public class Car
+        public Car(int passengers)
         {
-            public int Passengers { get; set; }
+            Passengers = passengers;
+        }
+
+        public int Passengers { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Car);
+        }
+
+        public bool Equals(Car other)
+        {
+            return other != null &&
+                   Passengers == other.Passengers;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Passengers);
+        }
+
+        public override string ToString()
+        {
+            return $"Car {{ {nameof(Passengers)} = {Passengers} }}";
+        }
+
+        public static bool operator ==(Car left, Car right)
+        {
+            return EqualityComparer<Car>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Car left, Car right)
+        {
+            return !(left == right);
         }
     }
+}
--- src/TollCollectorConsole/Program.cs
+++ src/TollCollectorConsole/Program.cs
@@ -21,7 +21,7 @@ namespace TollCollectorConsole
             Demo.Output();
 
             await TollSystem.ChargeTollAsync(
-                new Car { Passengers = 2 },
+                new Car(2),
                 time: DateTime.Now,
                 inbound: true,
                 license: "BSF-846-WA");
